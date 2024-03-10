c65de42d2635ce98aeb03a7434ff2a9b62f5dd03
Ajas Mohammed <ajasmohammed33@gmail.com>
2024-03-10 11:52:50 +0530

    Setup PONG so that whenever a client connects to the server PONG response is send back to the client.

--- app/main.py
+++ app/main.py
@@ -6,10 +6,12 @@ def main():
     # You can use print statements as follows for debugging, they'll be visible when running tests.
     print("Logs from your program will appear here!")
 
-    # Uncomment this to pass the first stage
-    #
+    pong = b"+PONG\r\n"
+
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
-    server_socket.accept() # wait for client
+    connection, address = server_socket.accept() # wait for client
+
+    connection.sendall(pong)
 
 
 if __name__ == "__main__":
