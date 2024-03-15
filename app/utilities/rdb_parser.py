c94fdce7da74c6c260e9ddb5086348f7a17d2f3d
Ajas Mohammed <ajasmohammed33@gmail.com>
2024-03-15 23:18:21 +0530

    Modified the DB Parser to read the expiry time of the the keys and return the valid key-value pair.

--- app/utilities/rdb_parser.py
+++ app/utilities/rdb_parser.py
@@ -1,15 +1,18 @@
+import time
 from .store import Store
 
 
 class DatabaseParser:
     def __init__(self) -> None:
-        self.key_value_pair : dict = {}
-        
+        self.key_value_pair: dict = {}
+
     def parse_lenght(self, data: bytes, current_index: int):
 
         byte = data[current_index]
         current_index += 1
-        ms2_bits = byte >> 6  # Right Shift 6bits to bring the most significant 2 bits to the last
+        ms2_bits = (
+            byte >> 6
+        )  # Right Shift 6bits to bring the most significant 2 bits to the last
         length = None
 
         if ms2_bits == 0b00:
@@ -45,14 +48,15 @@ class DatabaseParser:
 
     def database_parser(self, path: str):
         try:
-            with open(path, 'rb') as file:
+            with open(path, "rb") as file:
                 data = file.read()
         except:
             return None
-        
+
         magic, data = data[:5], data[5:]
         ver, data = data[:4], data[4:]
         current_index = 0
+        expire_time = None
 
         while current_index < len(data):
             op_code = data[current_index]
@@ -69,17 +73,18 @@ class DatabaseParser:
                 resizedb, current_index = self.parse_lenght(data, current_index)
                 resizedb, current_index = self.parse_lenght(data, current_index)
 
-            elif op_code == 0xFD:
+            elif op_code == 0xFD:  # Expiry time in seconds
                 expire_time = int.from_bytes(
                     data[current_index : current_index + 4], "little"
                 )
                 current_index += 4
 
-            elif op_code == 0xFC:
+            elif op_code == 0xFC:  # Expiry time in milliseconds
                 expire_time = int.from_bytes(
                     data[current_index : current_index + 8], "little"
                 )
                 current_index += 8
+                expire_time = expire_time / 1000
 
             elif op_code == 0xFF:
                 break
@@ -87,12 +92,19 @@ class DatabaseParser:
             elif op_code == 0x00:
                 key, current_index = self.parse_rdb_string(data, current_index)
                 value, current_index = self.parse_rdb_string(data, current_index)
-                self.key_value_pair[key.decode('utf-8')] = (value.decode('utf-8'), None)
+
+                if expire_time and expire_time > time.time():
+
+                    self.key_value_pair[key.decode("utf-8")] = (
+                        value.decode("utf-8"),
+                        expire_time,
+                    )
+
+                expire_time = None
             else:
                 continue
 
         return self.key_value_pair
-    
 
     def update_store(self, store: Store, path: str):
         self.database_parser(path)
