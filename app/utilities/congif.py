974c313e5c9864d9afbdab9b07c8d1e1f923fba5
Ajas Mohammed <ajasmohammed33@gmail.com>
2024-03-20 23:49:43 +0530

    Fixed a logic error in the empty_rdb method in Replication.

--- app/utilities/congif.py
+++ app/utilities/congif.py
@@ -23,8 +23,8 @@ class Replication:
     def empty_rdb(self):
         empty_rdb_hex = '524544495330303131fa0972656469732d76657205372e322e30fa0a72656469732d62697473c040fa056374696d65c26d08bc65fa08757365642d6d656dc2b0c41000fa08616f662d62617365c000fff06e3bfec0ff5aa2'
         empty_rdb = bytes.fromhex(empty_rdb_hex)
-        res = f'${len(empty_rdb)}\r\n{empty_rdb}'
-        return bytes(res, 'utf-8')
+        res = f"{len(empty_rdb)}\r\n".encode()
+        return res + empty_rdb
 
 
 @dataclass(kw_only=True)
